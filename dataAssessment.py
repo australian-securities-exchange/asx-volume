a1a0eae85463d065537ba0538bee59b297c2dab8
Gerard <gerardw3@outlook.com>
2020-10-15 13:55:34 +1100

    DataAssessment Created
    math to determine the number of standard deviations from mean of current day's data

---
+++ dataAssessment.py
@@ -0,0 +1,53 @@
+
+volData = [20255479,10917563,14103186,27241123,8163199]
+mean = 16136110
+sd = 6858918.825634781
+
+from math import sqrt
+
+#Mean and Standard Deviation Function
+#[mean,sd] = dataMean(data)
+#
+#Inputs:
+#-volume data array with 1(?) year of data
+#
+#Outputs:
+#-mean of data
+#-standard deviation
+#-current standard deviation from mean
+#-statistically relevancy
+
+def dataMean(data):
+	sum = 0
+	inner_sum = 0
+	
+	for i in range(0,len(data)):
+	    sum = sum + data[i]
+	
+	mean = int(sum/len(data))
+	#print(len(data))
+	#print(mean)
+	
+	for i in range(0,len(data)):
+	    interior = abs(data[i]-mean)
+	    inner_sum = inner_sum + interior**2
+	    
+	sd = sqrt(inner_sum/len(data))
+	
+	return (mean,sd)    #this isn't right, fix up for two output returns
+
+#Current Deviation Function
+#[numSD] = currentDayDeviation(data,mean,sd)
+#
+#Inputs:
+#-volume data array with 1(?) year of data
+#-mean calculated from previous function
+#-deviation calculated from previous function
+#
+#Outputs:
+#-number of standard deviations the current volume is from the mean over time period
+
+def currentDayDeviation(data,mean,sd):
+    numSD = (data[-1]-mean)/sd
+    return (numSD)
+    
